d3f3b6e5bd12887098041255533eeafe05087dd7
rishavroy97 <rishavroy008@gmail.com>
2024-05-06 22:04:18 -0400

    Read input

--- iosched.cpp
+++ iosched.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <cstring>
-#include <deque>
 
+using namespace std;
 
 class IO {
 public:
@@ -19,25 +19,33 @@ public:
 class Scheduler {
 public:
     virtual IO *get_next_io() = 0;
+
+    virtual ~Scheduler() = default;
 };
 
-class FCFSScheduler: public Scheduler {
+class FCFSScheduler : public Scheduler {
 public:
     IO *get_next_io() override {
-        return new IO(0,0);
+        return new IO(0, 0);
     }
 };
 
 /**
  * Global Variables
  */
-bool VERBOSE = false;
-bool SHOW_Q = false;
-bool SHOW_F = false;
-Scheduler *SCHEDULER = nullptr;
+bool VERBOSE = false;           // Show verbose execution trace
+bool SHOW_Q = false;            // Show verbose IO Queue Information
+bool SHOW_F = false;            // Show Additional information for FLOOK
+Scheduler *SCHEDULER = nullptr; // Scheduler instance being used in simulation
+vector<IO *> IO_REQUESTS;       // initialize a list of IO Requests
 
-
-Scheduler *getScheduler(char *optarg) {
+/**
+ * Get the appropriate scheduler based on the arguments
+ * @param - args - string that needs to parsed to fetch the scheduler type
+ *
+ * @returns - the correct Scheduler based on the arguments
+ */
+Scheduler *get_scheduler(char *args) {
     return nullptr;
 }
 
@@ -61,7 +69,7 @@ void read_arguments(int argc, char **argv) {
                 SHOW_F = true;
                 break;
             case 's': {
-                SCHEDULER = getScheduler(optarg);
+                SCHEDULER = get_scheduler(optarg);
                 break;
             }
             default:
@@ -80,16 +88,64 @@ void read_arguments(int argc, char **argv) {
     }
 }
 
+/**
+ * Parse the io information from the input file
+ * @param - filename - input file
+ */
+void load_io_requests(char *filename) {
+    fstream input_file;
+
+    input_file.open(filename, ios::in);
+
+    if (!input_file.is_open()) {
+        printf("Not a valid inputfile <%s>\n", filename);
+        exit(1);
+    }
+
+    string line;
+    while (getline(input_file, line)) {
+        if (line[0] == '#')
+            continue;
+        int arrival_time, access_track;
+        char *buffer = new char[line.length() + 1];
+        strcpy(buffer, line.c_str());
+        sscanf(buffer, "%d %d", &arrival_time, &access_track);
+        auto *io = new IO(arrival_time, access_track);
+        IO_REQUESTS.push_back(io);
+        delete[] buffer;
+    }
+}
+
+/**
+ * Debug function to pretty print the input tokens
+ */
+[[maybe_unused]] void print_input() {
+    int count = 0;
+    for (IO *io: IO_REQUESTS) {
+        printf("%5d: %5d %5d\n", count, io->arrival_time, io->access_track);
+    }
+}
+
+/**
+ * Deallocate memory used in the program
+ */
+void garbage_collection() {
+    delete SCHEDULER;
+
+    for (IO *io: IO_REQUESTS)
+        delete io;
+}
+
 int main(int argc, char **argv) {
     read_arguments(argc, argv);
-//    parse_randoms(argv[optind + 1]);
-//    load_processes(argv[optind]);
-//
-//    DISPATCHER = new DES_Layer();
-//    DISPATCHER->initialize(PROCESSES);
-//    run_simulation();
-//
-//    print_output();
-//
-//    garbage_collection();
+    load_io_requests(argv[optind]);
+    print_input();
+    //
+    //    DISPATCHER = new DES_Layer();
+    //    DISPATCHER->initialize(PROCESSES);
+    //    run_simulation();
+    //
+    //    print_output();
+    //
+    garbage_collection();
 }
